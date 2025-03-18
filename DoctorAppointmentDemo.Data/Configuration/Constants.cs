d7d876d9006bb66d728e9964dc706d9228246104
Mykyta Khomik <n.homick4@gmail.com>
2025-03-18 17:56:01 +0200

    Constants change path

--- DoctorAppointmentDemo.Data/Configuration/Constants.cs
+++ DoctorAppointmentDemo.Data/Configuration/Constants.cs
@@ -3,6 +3,8 @@
     public static class Constants
     {
         // заменить на путь валидный для вашей директории на пк (в будущем будем использовать относительный путь)
-        public const string AppSettingsPath = "C:\\Users\\admin\\source\\repos\\DoctorAppointmentDemo\\DoctorAppointmentDemo.Data\\Configuration\\appsettings.json";
+        public const string AppSettingsPath = "D:\\Projects\\Hillel\\DoctorAppointmentDemo\\DoctorAppointmentDemo.Data\\Configuration\\appsettings.json";
+
+
     }
 }
