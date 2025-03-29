022f1910e773c5e05f6fb7c21980e8b1d0d0f001
Mykyta Khomik <n.homick4@gmail.com>
2025-03-29 23:30:40 +0200

    change appsetings path

---
+++ DoctorAppointmentDemo.Service/Interfaces/ISerializationService.cs
@@ -0,0 +1,9 @@
+﻿namespace DoctorAppointmentDemo.Service.Interfaces
+{
+    public interface ISerializationService
+    {
+        void Serialize<T>(string path, T data);
+
+        T Deserialize<T>(string path);
+    }
+}
